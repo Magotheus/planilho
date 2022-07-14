f16e852c498723fd6d6eb71f16e84b9216a51d02
Matheus Soares <prog.mago@gmail.com>
2022-07-14 15:47:03 -0300

    ok

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include('conexao.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Planilho</title>
+    <title>Planilh</title>
 </head>
 <body>
     <h1>Planilho</h1>
